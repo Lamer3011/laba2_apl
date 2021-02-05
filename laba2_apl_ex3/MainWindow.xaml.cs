43d0a757b07e177e6ef1d5949046fa53f13ef551
Lamer Matrixxx <jssk383@gmail.com>
2021-02-05 16:49:14 +0200

    добавив 3 приклад

---
+++ laba2_apl_ex3/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba2_apl_ex3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void log_button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = user_log.Text;
+            string password = pass_log.Password;
+            if (username.Length <= 2)
+            {
+                MessageBox.Show("Введіть логін(більше 2 букв)");
+            } else if (password.Length <= 2)
+            {
+                MessageBox.Show("Введіть пароль(більше 2 букв)");
+            } else
+            {
+                if (username == "admin")
+                {
+                    if (password == "admin")
+                    {
+                        MessageBox.Show("Ви успішно увійшли");
+                    } else
+                    {
+                        MessageBox.Show("Неправильний пароль", "Error");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неправильний логін", "Error");
+                }
+            }
+        }
+    }
+}
